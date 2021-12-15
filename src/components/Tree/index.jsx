98b13ad405198d8fb94d1087b8fa345093ec2385
ruoshuikun <ruoshuikun@yeah.net>
2021-12-15 19:35:27 +0800

    新增tree组件；头部导航调整

---
+++ src/components/Tree/index.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+// import arrowDownGray from '../../assets/img/arraw-down-gray.png';
+// import arrowUpGray from '../../assets/img/arraw-up-gray.png';
+import arrowUpGray from '../../assets/img/arraw-down-gray.png';
+import arrowDownGray from '../../assets/img/arraw-right-gray.png';
+import './index.scss'
+
+class Tree extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            keys:[]
+        }
+    }
+    componentWillMount(){
+        const {defaultExpandedKeys,treeData,children} = this.props;
+        //处理默认样式
+        if(treeData && treeData.length>0){
+            if(Array.isArray(defaultExpandedKeys) && defaultExpandedKeys.length>0) {
+                treeData.map((item) => {
+                    if (defaultExpandedKeys.includes(String(item.key))) {
+                        item.isOpenChild = true;//展开子节点
+                    }
+                });
+            }
+        }
+        //处理自定义样式
+        if(children && children.length>0){
+            let keys = [];
+            children.map((item)=>{
+                keys.push(item.key)
+            });
+            if(Array.isArray(defaultExpandedKeys) && defaultExpandedKeys.length>0){
+                children.map((item1)=>{
+                    if(defaultExpandedKeys.includes(item1.key)){
+                        if(Array.isArray(item1.props.children) && item1.props.children.length>0){
+                            item1.props.children.map((item2)=>{
+                                keys.push(item2.key);
+                            });
+                        }
+                    }
+                });
+            }
+            this.setState({
+                keys
+            });
+        }
+    }
+    onTrigger(e,item){
+        e.stopPropagation();
+        let keys = this.state.keys;
+        if(item.props.children && item.props.children.length>0){
+            item.props.children.map((item1)=>{
+                //如果没有子节点就添加
+                if(!keys.includes(item1.key)){
+                    keys.push(item1.key);
+                }else {
+                    //否则过滤子节点
+                    keys = keys.filter(item2=>item2!=item1.key);
+                }
+            });
+        }
+        this.setState({
+            keys
+        })
+    }
+    onSelect(e,item){
+        const {onSelect} = this.props;
+        if(onSelect){
+            onSelect(item);
+        }
+    }
+    onTrigger1(e,item){
+        e.stopPropagation();
+        item.isOpenChild = !item.isOpenChild;
+        this.setState({
+            isRefresh:!this.state.isRefresh
+        })
+    }
+    loopTree(arr){
+        return  arr.map((item,index)=>{
+            return <div key={index} className='tree-wrap'>
+                {
+                    <div className="tree-title">
+                        <div className="cm-flex-1 cm-hover-bc-eee cm-p-01" onClick={(e)=>this.onSelect(e,item)}>{item.title}</div>
+                        {
+                            item.children?
+                                <div onClick={(e)=>this.onTrigger1(e,item)} className="cm-ml-01 cm-p-005">
+                                    <img src={item.isOpenChild?arrowUpGray:arrowDownGray}
+                                         className="tree-arrow" alt="" />
+                                </div> :null
+                        }
+                    </div>
+                }
+                {
+                    item.children&&item.children.length&&item.isOpenChild?
+                        <div className={item.isOpenChild?"cm-display-block ":"cm-display-none"}><div className="ml-default" >{this.loopTree(item.children)}</div></div> :null
+                }
+            </div>
+        })
+    }
+    loopChild(arr){
+        const {keys} = this.state;
+        return  arr.map((item,index)=>{
+            return <div key={index} className={keys.includes(item.key)?"cm-display-block":"cm-display-none"}>
+                {
+                    <div className="cm-flex cm-jc-sb cm-p-01 cm-ai-c cm-cursor-p">
+                        <div className="cm-flex-1" onClick={(e)=>this.onSelect(e,item)}>{item}</div>
+                        {
+                            item.props.children?
+                                <div onClick={(e)=>this.onTrigger(e,item)} className="cm-ml-01 cm-p-005">
+                                    <img src={keys.includes(item.props.children[0].key)?arrowUpGray:arrowDownGray}
+                                         className="tree-arrow" alt="" />
+                                </div> :null
+                        }
+                    </div>
+                }
+                {
+                    item.props.children&&item.props.children.length?
+                        <div className="ml-default">{this.loopChild(item.props.children)}</div>:null
+                }
+            </div>
+        })
+    }
+    render(){
+        const {treeData,children} = this.props;
+        return (
+            <>
+                {treeData && treeData.length>0?this.loopTree(treeData):this.loopChild(children)}
+            </>
+        )
+    }
+}
+class TreeNode extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const {title} = this.props;
+        return  <div className="cm-c-333">
+            {title}
+        </div>
+    }
+}
+Tree.TreeNode = TreeNode;
+export default Tree;
--- src/pages/LearnMore/index.js
+++ src/pages/LearnMore/index.js
@@ -1,7 +1,103 @@
 import React from "react";
+import './index.scss'
+import Tree from '../../components/Tree/index.jsx';
 
-export default class LearnMore extends React.Component{
-    render(){
-        return <div>LearnMore</div>
+export default class LearnMore extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            treeData: [
+                {
+                    title: 'Welcome to MosaicFantasy',
+                    key: '0-0',
+                },
+                {
+                    title: 'About MosaicFantasy Platform',
+                    key: '1-0',
+                    children: [
+                        {
+                            title: 'Blockchain Basis',
+                            key: '1-0-0',
+                        },
+                        {
+                            title: 'Wallet Guideline',
+                            key: '1-0-1',
+                        },
+                        {
+                            title: 'What’s Mosaic NFT?',
+                            key: '1-0-2',
+                        },
+                        {
+                            title: 'How to get Mosaic NFT?',
+                            key: '1-0-3',
+                        },
+                        {
+                            title: 'What’s mining power?',
+                            key: '1-0-4',
+                        },
+                    ],
+                },
+                {
+                    title: 'About MosaicFantasy Game',
+                    key: '2-0',
+                    children: [
+                        {
+                            title: 'Entering the game',
+                            key: '2-0-0',
+                        },
+                        {
+                            title: 'Background',
+                            key: '2-0-1',
+                        },
+                        {
+                            title: 'Introduction of Mosaic',
+                            key: '2-0-2',
+                            children:[
+                                {
+                                    title: 'Ocean Mosaic',
+                                    key: '2-0-2-0',
+                                },
+                                {
+                                    title: 'Storm Mosaic',
+                                    key: '2-0-2-1',
+                                },
+                                {
+                                    title: 'Flame Mosaic',
+                                    key: '2-0-2-2',
+                                },
+                                {
+                                    title: 'Dawn Mosaic',
+                                    key: '2-0-2-3',
+                                },
+                                {
+                                    title: 'Nether Mosaic',
+                                    key: '2-0-2-4',
+                                },
+                            ]
+                        },
+                        {
+                            title: 'Gameplay',
+                            key: '2-0-2',
+                        },
+                    ],
+                },
+            ]
+        }
+    }
+
+    render() {
+        const tree = this.state.treeData
+        return (
+            <div className='learn-more'>
+                <div className='learn-more-menu'>
+                    <Tree
+                        treeData={tree}
+                        onSelect={() => this.onSelect()}
+                        defaultExpandedKeys={['0-0-0']}
+                    />
+                </div>
+                <div className='learn-more-content'>main</div>
+            </div>
+        )
     }
 }
